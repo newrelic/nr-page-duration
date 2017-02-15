593900ddc1fb5d6d6225a4e23db678e6a09f3b0d
Blaine Schmeisser <blainesch@gmail.com>
2017-02-15 14:57:16 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+if (window) {
+  var called = false
+  var callback = function () {
+    if (called) return
+    called = true
+    var end = new Date().getTime()
+    var start = __nr_require('loader').offset
+    window.newrelic.addPageAction('PageDuration', {
+      duration: Math.round((end - start) / 1000),
+    })
+  }
+  if ('addEventListener' in window) {
+    window.addEventListener('pagehide', callback, true)
+    window.addEventListener('unload', callback, true)
+  } else if ('attachEvent' in window) {
+    window.attachEvent('onpagehide', callback)
+    window.attachEvent('onunload', callback)
+  }
+}
